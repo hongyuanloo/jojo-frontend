ee523bf9d91b8b38ab5a18871d571056d9707fef
Hong Yuan <loohongyuan5505@hotmail.com>
2023-02-16 01:34:04 +0800

    update redux store to redux persist store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit";
-import productsReducer from "./features/productsSlice";
-import cartReducer from "./features/cartSlice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import productsReducer from "./slices/productsSlice";
+import cartReducer from "./slices/cartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
+import thunk from "redux-thunk";
+
+// ref: https://blog.logrocket.com/persist-state-redux-persist-redux-toolkit-react/
+
+// configure redux persist
+const persistConfig = {
+  key: "root",
+  storage, // storage refers to local storage.
+  stateReconciler: autoMergeLevel2, // indicate how persisted data is merged into redux store on refresh.
+};
+
+// combine all reducers
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+});
+
+// return type of rootReducer
+type TrootReducer = ReturnType<typeof rootReducer>;
+
+// "enchanced" reducer with persist capability
+const persistedReducer = persistReducer<TrootReducer>(
+  persistConfig,
+  rootReducer
+);
 
 // setup redux store
 export const store = configureStore({
-  reducer: {
-    products: productsReducer,
-    cart: cartReducer,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
 // Infer reduxRootState and reduxDispatch types from store itself.
 export type reduxRootState = ReturnType<typeof store.getState>;
 export type reduxDispatch = typeof store.dispatch;
+
+// store persistor
+export const persistor = persistStore(store);
