1f5f8507ec786ba88f5845ec6bc87d60afefac3e
Hong Yuan <loohongyuan5505@hotmail.com>
2023-02-22 20:43:54 +0800

    update CategoriesContainer to display data from allProducts

--- src/components/CategoriesContainer/index.tsx
+++ src/components/CategoriesContainer/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import {
   Typography,
   Container,
@@ -6,34 +7,119 @@ import {
   CardActionArea,
   CardContent,
 } from "@mui/material";
+import { useReduxSelector } from "../../redux/hooks";
 import { CardStyled, TypographyStyled } from "../../styles/categoriesContainer";
 import { customColors } from "../../themes/customColors";
+import { TCategory } from "../../types_interfaces";
+import { CircularProgressLoading } from "../ProgressIndicator";
 
 export const CategoriesContainer = () => {
-  const data = [
-    {
-      name: "Clothes",
-      img: "https://api.lorem.space/image/fashion?w=640&h=480&r=3430",
-    },
-    {
-      name: "Electronics",
-      img: "https://api.lorem.space/image/watch?w=640&h=480&r=7796",
-    },
-    {
-      name: "Furniture",
-      img: "https://api.lorem.space/image/furniture?w=640&h=480&r=423",
-    },
-    {
-      name: "Shoes",
-      img: "https://api.lorem.space/image/shoes?w=640&h=480&r=5953",
-    },
-    {
-      name: "Others",
-      img: "https://api.lorem.space/image?w=640&h=480&r=5637",
-    },
-  ];
+  // ! for development use only.
+  // const data = [
+  //   {
+  //     name: "Clothes",
+  //     img: "https://api.lorem.space/image/fashion?w=640&h=480&r=3430",
+  //   },
+  //   {
+  //     name: "Electronics",
+  //     img: "https://api.lorem.space/image/watch?w=640&h=480&r=7796",
+  //   },
+  //   {
+  //     name: "Furniture",
+  //     img: "https://api.lorem.space/image/furniture?w=640&h=480&r=423",
+  //   },
+  //   {
+  //     name: "Shoes",
+  //     img: "https://api.lorem.space/image/shoes?w=640&h=480&r=5953",
+  //   },
+  //   {
+  //     name: "Others",
+  //     img: "https://api.lorem.space/image?w=640&h=480&r=5637",
+  //   },
+  // ];
 
-  // TODO OnClick : redirect to products filtered with that category
+  // interface for categories datum
+  interface IcategoriesDatum {
+    name: TCategory | "";
+    images: string[];
+  }
+
+  // store categoriesData
+  const [categoriesData, setCategoriesData] = useState<IcategoriesDatum[]>([]);
+
+  // allProducts state from redux store
+  const allProducts = useReduxSelector((state) => state.products.allProducts);
+
+  // update categoriesData whenever allProducts is updated
+  useEffect(() => {
+    // store visited categories
+    const visitedCategories: TCategory[] = [];
+    // store new categoriesData temporary
+    const tempCategoriesData: IcategoriesDatum[] = [];
+
+    // process new allProducts and update tempCategoriesData
+    allProducts.forEach((product) => {
+      const tempCategoryDatum: IcategoriesDatum = { name: "", images: [] };
+      const { categories, images } = product;
+
+      let index = visitedCategories.findIndex(
+        (category) => category === categories[0]
+      );
+
+      // category already visited
+      if (index !== -1) return;
+
+      // category not visited yet;
+      tempCategoryDatum.name = categories[0];
+      tempCategoryDatum.images.push(images[0]);
+      // update visited category
+      visitedCategories.push(categories[0]);
+      // update visited category
+      tempCategoriesData.push({ ...tempCategoryDatum });
+    });
+
+    // update categoriesData with tempCategoriesData
+    setCategoriesData([...tempCategoriesData]);
+  }, [allProducts]);
+
+  // display all category card as grid items
+  function DisplayCategories() {
+    const categoryGridItems = categoriesData.map((category) => {
+      return (
+        // Container for one category card
+        <Grid
+          item
+          mobile={6}
+          tablet={4}
+          // sx={{ border: "2px solid red" }}
+          padding={1}
+          key={category.images[0]}
+        >
+          <CardStyled>
+            {/* handle click on card */}
+            <CardActionArea
+              onClick={() => {
+                console.log(`category: ${category.name}`);
+              }}
+            >
+              {/* card image */}
+              <CardMedia
+                component="img"
+                image={category.images[0]}
+                alt={`image of ${category.name}`}
+                sx={{ height: { tablet: "180px", mobile: "140px" } }}
+              />
+              {/* card title */}
+              <CardContent sx={{ p: 1 }}>
+                <TypographyStyled>{category.name}</TypographyStyled>
+              </CardContent>
+            </CardActionArea>
+          </CardStyled>
+        </Grid>
+      );
+    });
+    return categoryGridItems;
+  }
 
   return (
     // main Categories Container
@@ -53,98 +139,17 @@ export const CategoriesContainer = () => {
         Categories
       </Typography>
 
-      {/* Container for all category cards */}
-      <Grid container rowSpacing={0} columnSpacing={0}>
-        {/* Container for one category card */}
-        <Grid
-          item
-          mobile={6}
-          tablet={4}
-          // sx={{ border: "2px solid red" }}
-          padding={1}
-        >
-          <CardStyled>
-            {/* handle click on card */}
-            <CardActionArea onClick={() => {}}>
-              {/* card image */}
-              <CardMedia
-                component="img"
-                image={data[0].img}
-                alt="green iguana"
-                sx={{ height: { tablet: "180px", mobile: "140px" } }}
-              />
-              {/* card title */}
-              <CardContent sx={{ p: 1 }}>
-                <TypographyStyled>{data[0].name}</TypographyStyled>
-              </CardContent>
-            </CardActionArea>
-          </CardStyled>
+      {/* if categoriesData is empty, show progress loader */}
+      {/* if categoriesData is not empty, show data */}
+      {categoriesData.length === 0 ? (
+        CircularProgressLoading()
+      ) : (
+        //  Container for all category cards
+        <Grid container rowSpacing={0} columnSpacing={0}>
+          {/* display all category cards */}
+          {DisplayCategories()}
         </Grid>
-        <Grid item mobile={6} tablet={4} padding={1}>
-          <CardStyled>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="180"
-                image={data[1].img}
-                alt="green iguana"
-                sx={{ height: { tablet: "180px", mobile: "140px" } }}
-              />
-              <CardContent sx={{ p: 1 }}>
-                <TypographyStyled>{data[1].name}</TypographyStyled>
-              </CardContent>
-            </CardActionArea>
-          </CardStyled>
-        </Grid>
-        <Grid item mobile={6} tablet={4} padding={1}>
-          <CardStyled>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="180"
-                image={data[2].img}
-                alt="green iguana"
-                sx={{ height: { tablet: "180px", mobile: "140px" } }}
-              />
-              <CardContent sx={{ p: 1 }}>
-                <TypographyStyled>{data[2].name}</TypographyStyled>
-              </CardContent>
-            </CardActionArea>
-          </CardStyled>
-        </Grid>
-        <Grid item mobile={6} tablet={4} padding={1}>
-          <CardStyled>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="180"
-                image={data[3].img}
-                alt="green iguana"
-                sx={{ height: { tablet: "180px", mobile: "140px" } }}
-              />
-              <CardContent sx={{ p: 1 }}>
-                <TypographyStyled>{data[3].name}</TypographyStyled>
-              </CardContent>
-            </CardActionArea>
-          </CardStyled>
-        </Grid>
-        <Grid item mobile={6} tablet={4} padding={1}>
-          <CardStyled>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="180"
-                image={data[4].img}
-                alt="green iguana"
-                sx={{ height: { tablet: "180px", mobile: "140px" } }}
-              />
-              <CardContent sx={{ p: 1 }}>
-                <TypographyStyled>{data[4].name}</TypographyStyled>
-              </CardContent>
-            </CardActionArea>
-          </CardStyled>
-        </Grid>
-      </Grid>
+      )}
     </Container>
   );
 };
